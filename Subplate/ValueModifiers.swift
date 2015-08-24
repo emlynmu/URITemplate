b3ad037b27d0e45163a996ec6cdbfc67b34bd399
Emlyn Murphy <me@emlyn.net>
2015-08-24 06:37:35 -0400

    Define value modifier types as an enum

---
+++ Subplate/ValueModifiers.swift
@@ -0,0 +1,14 @@
+//
+//  ValueModifiers.swift
+//  Subplate
+//
+//  Created by Emlyn Murphy on 8/24/15.
+//  Copyright (c) 2015 Emlyn Murphy. All rights reserved.
+//
+
+import Foundation
+
+enum ValueModifier {
+    case Prefix
+    case Composite
+}
