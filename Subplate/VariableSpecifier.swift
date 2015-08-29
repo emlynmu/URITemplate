a46d5be443a66e26f08b4bd2da9defcc3e9ee538
Emlyn Murphy <me@emlyn.net>
2015-08-29 18:41:38 -0400

    Move value expansion to variable specifier type.

--- Subplate/VariableSpecifier.swift
+++ Subplate/VariableSpecifier.swift
@@ -25,4 +25,79 @@ public struct VariableSpecifier: DebugPrintable {
             return name
         }
     }
+
+    private func prefix(expression: ExpressionType) -> String {
+        switch expression {
+        case .SimpleString, .Reserved:
+            return ""
+
+        case .Fragment:
+            return "#"
+
+        case .Label:
+            return "."
+
+        case .PathSegment:
+            return "/"
+
+        case .PathStyle:
+            return ";" + name + "="
+
+        case .FormStyleQuery:
+            return "?" + name + "="
+
+        case .FormStyleQueryContinuation:
+            return "&" + name + "="
+        }
+    }
+
+    private func listSeparator(expression: ExpressionType) -> String {
+        if let modifier = valueModifier {
+            switch modifier {
+            case .Composite:
+                switch expression {
+                case .SimpleString, .Reserved, .Fragment:
+                    break // default
+
+                case .Label:
+                    return "."
+
+                case .PathSegment:
+                    return "/"
+
+                case .PathStyle:
+                    return ";" + name + "="
+
+                case .FormStyleQuery, .FormStyleQueryContinuation:
+                    return "&" + name + "="
+                }
+
+            case .Prefix:
+                break  // default
+            }
+        }
+
+        return "," // default
+    }
+
+    public func expand(value: AnyObject?, inExpression expression: ExpressionType) -> String {
+        if let values = value as? [AnyObject] {
+            if values.count > 0 {
+                let separator = listSeparator(expression)
+
+                return prefix(expression) + values[1..<values.count].reduce(values[0].description) { (result: String, value: AnyObject) -> String in
+                    return result + separator + value.description
+                }
+            }
+            else {
+                return ""
+            }
+        }
+        else if let value: AnyObject = value {
+            return prefix(expression) + value.description
+        }
+        else {
+            return prefix(expression)
+        }
+    }
 }
